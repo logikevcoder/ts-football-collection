121cf048bfdedc11e09350f0d93b25777084da69
Kevin Napier <kevin.napier@rotary.org>
2020-08-03 10:59:15 -0500

    added initial code which represents an incorrect way of displaying some results from the csv file

--- build/index.js
+++ build/index.js
@@ -1,2 +1,28 @@
 "use strict";
-console.log('test');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs_1 = __importDefault(require("fs"));
+// use this to read any type of file
+// encoding says to return a string
+var matches = fs_1.default
+    .readFileSync('football.csv', { encoding: 'utf-8' })
+    .split('\n')
+    .map(function (row) { return row.split(','); });
+var manUnitedWins = 0;
+var homeWin = 'H';
+var awayWin = 'A';
+var draw = 'D';
+// BAD VERSION OF CODE
+// iterate over strings from the football.csv file containing Man United and add to the manUnitedWins if match[5] contains H or A
+for (var _i = 0, matches_1 = matches; _i < matches_1.length; _i++) {
+    var match = matches_1[_i];
+    if (match[1] === 'Man United' && match[5] === homeWin) {
+        manUnitedWins++;
+    }
+    else if (match[2] === 'Man United' && match[5] === awayWin) {
+        manUnitedWins++;
+    }
+}
+console.log("Manchester United won " + manUnitedWins + " games");
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log('test');
+import fs from 'fs';
+
+// use this to read any type of file
+// encoding says to return a string
+const matches = fs
+  .readFileSync('football.csv', { encoding: 'utf-8' })
+  .split('\n')
+  .map((row: string): string[] => row.split(','));
+
+let manUnitedWins = 0;
+const homeWin = 'H';
+const awayWin = 'A';
+const draw = 'D';
+
+// BAD VERSION OF CODE
+// iterate over strings from the football.csv file containing Man United and add to the manUnitedWins if match[5] contains H or A
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === homeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === awayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Manchester United won ${manUnitedWins} games`);
